2c35a9c2ce96fe18608c4e151bec37b98266d872
rahathasanDev <devrahat.hasan@gmail.com>
2024-11-24 12:34:48 +0600

    shadcn tailwind installaition

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
        <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Todo Redux App
     </h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
