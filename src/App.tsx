8360e259c0914eeb4cb87ea11df7a4ef5d48c71b
rahathasanDev <devrahat.hasan@gmail.com>
2024-11-25 23:56:14 +0600

    todo container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import Todo from "./pages/Todo";
 
 function App() {
-
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Todo Redux App
-    </h1>
-    <div>
-      <Button>Click me</Button>
-    </div>
-    </>
-  )
+      <h1 className="text-3xl font-bold underline">Todo Redux App</h1>
+      <Todo></Todo>
+    </> 
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Todo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <div>
+      <h1>ToDO</h1>
+    </div>
+  );
+};
+
+export default Todo;
