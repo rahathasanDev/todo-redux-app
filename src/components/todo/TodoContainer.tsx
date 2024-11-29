c3a8e84eaef0a38d68574f795aa2879b501986f7
rahathasanDev <devrahat.hasan@gmail.com>
2024-11-29 12:07:30 +0600

    AddTodo FIlter comp. done

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,19 +1,22 @@
 import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
   return (
     <div>
       <div className="flex justify-between mb-5">
-        <Button className="bg-primary-gradient text-xl font-semibold">Add todo</Button>
-        <Button className="text-xl font-semibold">Filter</Button>
+       
+        <AddTodoModal></AddTodoModal>
+        <TodoFilter></TodoFilter>
       </div>
       <div className="bg-primary-gradient w-full h-full  rounded p-[4px] ">
         <div className="bg-white p-5 space-y-3">
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
         </div>
       </div>
     </div>
