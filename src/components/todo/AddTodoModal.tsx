8c83ae32e78fd1529d8e4156a9a64ee05ecd2668
rahathasanDev <devrahat.hasan@gmail.com>
2024-11-29 12:08:24 +0600

    ModalTodo done

---
+++ src/components/todo/AddTodoModal.tsx
@@ -0,0 +1,52 @@
+import { Dialog } from '@radix-ui/react-dialog';
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { Button } from '../ui/button';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+
+const AddTodoModal = () => {
+  return (
+    <div>
+     <Dialog>
+      <DialogTrigger asChild>
+      <Button className="bg-primary-gradient text-xl font-semibold">
+          Add todo
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Add your task that you want to finish.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Task
+            </Label>
+            <Input
+              id="task"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Description
+            </Label>
+            <Input
+              id="description"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+    </div>
+  );
+};
+
+export default AddTodoModal;
