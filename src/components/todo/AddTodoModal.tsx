112e7e1a62f959d63cdc512eba081ce4c9f6b4a7
rahathasanDev <devrahat.hasan@gmail.com>
2024-12-03 17:08:44 +0600

    onsubmit , eventhandler and value function done

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -1,50 +1,73 @@
-import { Dialog } from '@radix-ui/react-dialog';
-import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
-import { Button } from '../ui/button';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
+import { Dialog, DialogClose } from "@radix-ui/react-dialog";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { FormEvent, useState } from "react";
 
 const AddTodoModal = () => {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ task, description });
+  };
+
   return (
     <div>
-     <Dialog>
-      <DialogTrigger asChild>
-      <Button className="bg-primary-gradient text-xl font-semibold">
-          Add todo
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Add your task that you want to finish.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Task
-            </Label>
-            <Input
-              id="task"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Description
-            </Label>
-            <Input
-              id="description"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="bg-primary-gradient text-xl font-semibold">
+            Add todo
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="sm:max-w-[425px]">
+          <form onSubmit={onSubmit}>
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Add your task that you want to finish.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Task
+                </Label>
+                <Input
+                  onBlur={(e) => setTask(e.target.value)}
+                  id="task"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Description
+                </Label>
+                <Input
+                  onBlur={(e) => setDescription(e.target.value)}
+                  id="description"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <div>
+              <DialogClose asChild>
+                <Button type="submit">Save changes</Button>
+              </DialogClose>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
